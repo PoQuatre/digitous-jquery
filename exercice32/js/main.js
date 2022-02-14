756260fcd70d2f255d332d317207cb0cea8a187a
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-14 13:59:04 +0100

    Added exercise 32

---
+++ exercice32/js/main.js
@@ -0,0 +1,8 @@
+$("button.btn.btn-success").click(() => {
+  $.ajax({
+    url: "https://restcountries.com/v3.1/name/france",
+    success: function (data, statuts, response) {
+      console.log(data[0].name.common, "-", data[0].capital[0]);
+    },
+  });
+});
